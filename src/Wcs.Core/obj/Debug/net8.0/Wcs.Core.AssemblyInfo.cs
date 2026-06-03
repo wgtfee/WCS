8ecd03505f5ebf9af7318034b5c685e797861ad8
Crash.Y <897320499@qq.com>
2026-06-03 13:18:16 +0800

    V10test

--- src/Wcs.Core/obj/Debug/net8.0/Wcs.Core.AssemblyInfo.cs
+++ src/Wcs.Core/obj/Debug/net8.0/Wcs.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wcs.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5f3989aee6ea649e801480031b4fd3bed17ddb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+077bd444fc0c5fb9788c5ac1e0e63d7c8f570ed4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wcs.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wcs.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Wcs.Desktop/obj/Debug/net8.0/Wcs.Desktop.AssemblyInfo.cs
+++ src/Wcs.Desktop/obj/Debug/net8.0/Wcs.Desktop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wcs.Desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5f3989aee6ea649e801480031b4fd3bed17ddb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+077bd444fc0c5fb9788c5ac1e0e63d7c8f570ed4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wcs.Desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wcs.Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
