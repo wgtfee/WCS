aa36adba68f8c95e8e491e3d32e3a4cb3eec4a75
Crash.Y <57023277+wgtfee@users.noreply.github.com>
2026-08-03 01:01:10 +0800

    feat(simulation): add S8 virtual endurance constants

---
+++ src/Wcs.Simulator/CapacityReadiness/CapacityReadinessConstants.cs
@@ -0,0 +1,7 @@
+namespace Wcs.Simulator.CapacityReadiness;
+
+public static class CapacityReadinessConstants
+{
+    public const long EightHoursMilliseconds = 28_800_000;
+    public const long TwentyFourHoursMilliseconds = 86_400_000;
+}
