ee586f5cbb4d16b373df0119a9ceee34e2719b7d
Crash.Y <57023277+wgtfee@users.noreply.github.com>
2026-08-03 02:11:55 +0800

    refactor: remove unused S8 module marker

--- src/Wcs.Simulator/CapacityReadiness/CapacityReadinessModule.cs
+++
@@ -1,7 +0,0 @@
-namespace Wcs.Simulator.CapacityReadiness;
-
-public static class CapacityReadinessModule
-{
-    public const string SectionName = CapacityReadinessOptions.SectionName;
-    public const bool SimulationOnly = true;
-}
