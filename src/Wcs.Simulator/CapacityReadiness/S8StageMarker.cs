996c4394717acafab5b291e6dea4de57a9b6e7a3
Crash.Y <57023277+wgtfee@users.noreply.github.com>
2026-08-03 01:18:16 +0800

    refactor: remove stale S8 stage marker

--- src/Wcs.Simulator/CapacityReadiness/S8StageMarker.cs
+++
@@ -1,7 +0,0 @@
-namespace Wcs.Simulator.CapacityReadiness;
-
-internal static class S8StageMarker
-{
-    internal const string Stage = "S8";
-    internal const string Scope = "CapacityLongStabilityAndHilReadiness";
-}
