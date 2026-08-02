8a3d5dd19650eeb0b89f2a99c324c863c6b50ef7
Crash.Y <57023277+wgtfee@users.noreply.github.com>
2026-08-03 02:22:32 +0800

    refactor: keep S8 terminal conservation rules in runtime

--- src/Wcs.Simulator/CapacityReadiness/CapacityReadinessThresholds.cs
+++
@@ -1,8 +0,0 @@
-namespace Wcs.Simulator.CapacityReadiness;
-
-public static class CapacityReadinessThresholds
-{
-    public const int AllowedTerminalReservations = 0;
-    public const int AllowedTerminalWaitingRequests = 0;
-    public const int AllowedTerminalDeadlocks = 0;
-}
