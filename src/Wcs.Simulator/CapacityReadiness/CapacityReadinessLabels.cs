87637e30538858d3b03a5408d00eaaec8de05ab4
Crash.Y <57023277+wgtfee@users.noreply.github.com>
2026-08-03 01:18:33 +0800

    refactor: remove stale S8 labels helper

--- src/Wcs.Simulator/CapacityReadiness/CapacityReadinessLabels.cs
+++
@@ -1,9 +0,0 @@
-namespace Wcs.Simulator.CapacityReadiness;
-
-public static class CapacityReadinessLabels
-{
-    public const string Capacity = "capacity";
-    public const string EightHour = "virtual-8h";
-    public const string TwentyFourHour = "virtual-24h";
-    public const string HilGate = "hil-readiness";
-}
