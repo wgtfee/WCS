e3cc3fed4889712c7bd8092231fe5bdf8cc5e1c9
Crash.Y <57023277+wgtfee@users.noreply.github.com>
2026-08-03 02:11:38 +0800

    refactor: remove unused S8 constants wrapper

--- src/Wcs.Simulator/CapacityReadiness/CapacityReadinessConstants.cs
+++
@@ -1,7 +0,0 @@
-namespace Wcs.Simulator.CapacityReadiness;
-
-public static class CapacityReadinessConstants
-{
-    public const long EightHoursMilliseconds = 28_800_000;
-    public const long TwentyFourHoursMilliseconds = 86_400_000;
-}
