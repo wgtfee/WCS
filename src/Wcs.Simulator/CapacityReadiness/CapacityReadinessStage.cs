ff8684ccd11f5262035c090e60c9f568fcb8fe27
Crash.Y <57023277+wgtfee@users.noreply.github.com>
2026-08-03 02:12:10 +0800

    refactor: remove unused S8 stage marker

--- src/Wcs.Simulator/CapacityReadiness/CapacityReadinessStage.cs
+++
@@ -1,7 +0,0 @@
-namespace Wcs.Simulator.CapacityReadiness;
-
-public static class CapacityReadinessStage
-{
-    public const string Name = "S8 Capacity Long Stability / HIL Readiness";
-    public const string Version = "1.0";
-}
